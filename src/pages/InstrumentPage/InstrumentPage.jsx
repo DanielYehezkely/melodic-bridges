f6b1e8d2369bb42211040cb76fb7865022e5e1d5
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2024-07-15 02:49:23 +0300

    Done with the instrument playlist

--- src/pages/InstrumentPage/InstrumentPage.jsx
+++ src/pages/InstrumentPage/InstrumentPage.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { useAuth } from '../../context/useAuthContext'
 import InstrumentCard from "../../components/InstrumentCard/InstrumentCard";
 import InstrumentGallery from "../../components/InsrumentGallery/InstrumentGallery";
 import useInstruments from "../../hooks/useInstruments";
+import InstrumentsPlaylist from "../../components/InstrumentsPlaylist/InstrumentsPlaylist";
 import "./InstrumentPage.css";
+import { addSongToFavorites } from "../../services/firebase/usersService";
 
 const InstrumentPage = () => {
   const { id } = useParams();
   const { instruments, loading, error } = useInstruments();
+  const { user } = useAuth()
 
   if (loading) {
     return <div>Loading...</div>;
@@ -26,6 +30,11 @@ const InstrumentPage = () => {
     <div className="instrument-page">
       <InstrumentCard instrument={instrument} />
       <InstrumentGallery instrument={instrument} />
+      <InstrumentsPlaylist
+        playlist={instrument.playlist}
+        addSongToFavorites={addSongToFavorites}
+        userId={user.id} // Replace with actual user ID
+      />
     </div>
   );
 };
