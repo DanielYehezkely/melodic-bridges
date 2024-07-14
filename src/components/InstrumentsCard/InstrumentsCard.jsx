5ceb3cc21dd731c3906d7fa6274a9057dca1c557
Draz6 <163674931+Draz6@users.noreply.github.com>
2024-07-14 22:40:00 +0300

    update style

--- src/components/InstrumentsCard/InstrumentsCard.jsx
+++ src/components/InstrumentsCard/InstrumentsCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../styles/InstrumentsCard.css";
+import "../InstrumentsCard/InstrumentsCard.css";
 
 function InstrumentsCard(props) {
   const { image, name, description } = props;
