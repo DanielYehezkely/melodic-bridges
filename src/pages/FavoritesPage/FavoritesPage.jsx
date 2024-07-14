9ea5cbd87ac8ecb655890c4557098358d63d93c6
Evyatar Ifrach <evyatarevi@gmail.com>
2024-07-14 17:39:06 +0300

    to daniel

--- src/pages/FavoritesPage/FavoritesPage.jsx
+++ src/pages/FavoritesPage/FavoritesPage.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
 const FavoritesPage = () => {
+  const [playList, setPlayList] = useState();
+  const [favoriteSongs, setFavoriteSongs] = useState();
+  const [filteredSongs, setFilteredSongs] = useState();
+
+  useEffects(() => {
+    const fetchUserData = async () => {
+      try {
+        const playListRes = await // fetch playlist
+        setPlayList(playListRes.data);
+        const res = await // get user
+        setFavoriteSongs(res.favorites);
+      } catch (error) {}
+    };
+    fetchUserData();
+  }, []);
+
+  useEffect(() => {
+    setFilteredSongs(favoriteSongs.filter((favSong) => favSong.id === song.id));
+  }, []);
+
   return <div>FavoritesPage</div>;
 };
 
 export default FavoritesPage;
+
+// userData.favorite
+// allSongsPlaylist
