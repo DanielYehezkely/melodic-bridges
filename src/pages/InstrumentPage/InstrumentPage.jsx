f387fafedf5cdf77e2ffe18d254dd7171731dad2
liber1988 <92869703+liber1988@users.noreply.github.com>
2024-07-14 15:29:57 +0300

    I am adding InstrumenyCard InstrumentGallery and InstrumentPage components includeing there css files

--- src/pages/InstrumentPage/InstrumentPage.jsx
+++ src/pages/InstrumentPage/InstrumentPage.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import InstrumentCard from "../../components/InstrumentCard/InstrumentCard";
+import InstrumentGallery from "../../components/InsrumentGallery/InstrumentGallery";
+import useInstruments from "../../hooks/useInstruments";
+import "./InstrumentPage.css";
 
 const InstrumentPage = () => {
-  return <div>InstrumentPage</div>;
+  const { id } = useParams();
+  const { instruments, loading, error } = useInstruments();
+  const instrument = instruments.find((inst) => inst.id === id);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  if (!instrument) {
+    return <div>Instrument not found</div>;
+  }
+
+  return (
+    <div className="instrument-page">
+      <InstrumentCard instrument={instrument} />
+      <InstrumentGallery images={instrument.images} />
+    </div>
+  );
 };
 
 export default InstrumentPage;
